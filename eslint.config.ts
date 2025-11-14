4b684acf664b7ab4af1eae3f6837aef4e1d2d9de
Christopher Buss <christopher.buss+github@protonmail.com>
2025-11-14 01:05:41 +0000

    feat: add initial types

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,9 @@
-import { isentinel } from "@isentinel/eslint-config";
+import {  isentinel } from "@isentinel/eslint-config";
 
 export default isentinel({
 	formatters: {
 		lua: false,
 	},
-	ignores: ["**/*", "!**/*.d.ts", "!eslint.config.ts", "!package.json", "!.gitignore"],
+
 	type: "package",
 });
